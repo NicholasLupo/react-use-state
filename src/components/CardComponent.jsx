fb11af72a0a228c9639a230d4abcfc546fef612e
NicholasLupo <nicholaslupo14@gmail.com>
2026-04-30 16:07:52 +0200

    refactor CardComponent to dynamically render languages from data

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,14 +1,20 @@
+import languages from '../data/languages'
+
 export default function CardComponent() {
-
-
 
     return (
 
         <div className="container">
-            <div className="card p-3">
-                <button className="btn btn-primary"> Language </button>
-                <p> Text </p>
-            </div>
+            {
+                languages.map((item) => (
+
+                    <div className="card p-3 mb-3" key={item.id}>
+                        <button className="btn btn-primary"> {item.title} </button>
+                        <p> {item.description} </p>
+                    </div>
+
+                ))
+            }
         </div>
 
     )
